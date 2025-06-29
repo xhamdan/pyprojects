62c2b629d1ae9cca22bc1be8cae62f490c728744
xhamdan <magezi.sh@gmail.com>
2025-06-29 17:50:15 +0300

    indexing

---
+++ index.py
@@ -0,0 +1,12 @@
+students = []
+no_of_students = int(input("Enter no. of students: "))
+index = 0
+count = 1
+for no in range (no_of_students): 
+    student = input("Enter student name: ")
+    students = []
+    for student in students:
+        students.append(student)
+        print(f"Number {count} is:  {students[index]}")
+
+index+=1

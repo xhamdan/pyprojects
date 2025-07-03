e53946083b7b00e1c93f607124a046c1925abaee
xhamdan <magezi.sh@gmail.com>
2025-07-03 19:03:52 +0300

    extend method

--- append_list.py
+++ append_list.py
@@ -1,7 +1,17 @@
 #syntax: list.append()
+#append() adds an element to the end of the list
 languages=['c','cpp','java']
 print(languages)
 languages.append('python')
 print(languages)
 languages.append('ruby')
+print(languages)
+
+#insert method adds an item at a specificied position in a list
+languages.insert(0,'Php')
+print(languages)
+
+#We can also use extend method.
+more_languages = ["css","reactpy"]
+languages.extend(more_languages)
 print(languages)

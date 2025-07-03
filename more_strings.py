85fa6fa7d2246817161d928ffdfeaa471a51739e
xhamdan <magezi.sh@gmail.com>
2025-07-03 12:54:22 +0300

    more strings

---
+++ more_strings.py
@@ -0,0 +1,11 @@
+my_string="Parliament"
+#print(my_string[6])
+#Slicing in PYTHON Programming - Helps to access a range of characters from a string
+#start_index:end_index
+print("||"*20)
+print(my_string[:6]) #This is termed as slicing
+print(my_string + "Test")
+print(f'My String has {len(my_string)} characters')
+print(my_string.replace("ment", "male"))
+print(f'In the word {my_string} there are: {my_string.count("a")} "as" altogether')
+print(f'Letter "t" is at position {my_string.find("t")} in the Word {my_string}')

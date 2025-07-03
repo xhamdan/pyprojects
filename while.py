e83496040c727e61a69bfe12f745ed1c4fc73790
xhamdan <magezi.sh@gmail.com>
2025-07-03 18:02:11 +0300

    while1

---
+++ while.py
@@ -0,0 +1,16 @@
+#Remember the 25 laps. One wd have a start/initial point, the target and condition
+i = 0 #Loop counter i.e. Initial or starting value
+while i <5: #repeatition condition
+    i +=1    #performs the counting
+    print(i)
+
+#Print numbers 1,2,3 on the screen:
+"""n = 1
+while n < 10:
+    print(n)
+    n +=1"""
+#another example
+n = 100
+while True:
+    print(n)
+    n-=1

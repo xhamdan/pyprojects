4a8187c01a7933557ae5e6e462470487d1bb68ca
xhamdan <magezi.sh@gmail.com>
2025-07-03 13:06:22 +0300

    how to formulate a regno

---
+++ regno.py
@@ -0,0 +1,6 @@
+student_name = input("Enter student name:")
+first_three_characters=student_name[:3]
+number_of_characters=len(student_name)
+
+print(f'{first_three_characters}{number_of_characters}')
+

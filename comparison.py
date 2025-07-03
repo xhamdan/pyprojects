a6f6296345a0133f298745de6dc34721f2eac604
xhamdan <magezi.sh@gmail.com>
2025-07-03 11:20:21 +0300

    comparison operators

---
+++ comparison.py
@@ -0,0 +1,9 @@
+#Understanding Comparison operators.
+x= 13
+y= 33
+
+print(f'Equivalent: {x==y} \n Greater: {x >y} \n Not Equal to: {x!=y}')
+
+#Logical AND and the Logical OR and Logical NOT
+strip = 'hihihihihi'
+print(strip[1::2])

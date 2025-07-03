eb05383087078508023dfdc0af264e7e83501f30
xhamdan <magezi.sh@gmail.com>
2025-07-03 13:07:49 +0300

    a statement in python

---
+++ statements.py
@@ -0,0 +1,8 @@
+#A statement is an instruction for a computer to do something
+name = 'sam'
+print(name)
+
+#more
+x = 10  #statement
+x = x + 3 #statement : (x+3) is an expression
+#Multi-line statements can be extended by using \, (), {}, []

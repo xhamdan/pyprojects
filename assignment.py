a02e6b632ee42766ff6fa3df635cbc941e85fcad
xhamdan <magezi.sh@gmail.com>
2025-07-03 11:07:20 +0300

    assignment

---
+++ assignment.py
@@ -0,0 +1,26 @@
+word = "Football"
+letter = "Footbal"
+print(word != letter)
+
+#operator precendence
+
+str = "hello"
+print(id(str))
+
+x = 10
+y = 20 
+z = 30 
+if x > y and y > z:
+    print('x is the greatest number')
+else:
+    print('x is not the greatest number')
+
+#More examples
+x = 'apple'
+y = 'apricot'
+print(x >= y)
+
+x = 55
+y = 10
+x &= y
+print(x)

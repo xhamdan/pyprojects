e5c63631da21ac07bc2e3b1373baf5fb1d242093
xhamdan <magezi.sh@gmail.com>
2025-07-03 18:01:04 +0300

    variables

---
+++ variables.py
@@ -0,0 +1,21 @@
+#declaring and assigning variables
+student_name='paul'
+student_age = 30
+student_height = 1.7
+
+#using and calling a variable
+print(f'{student_name} is {student_age} years old with height {student_height} meters')
+#MORE Variables
+var_one = 10
+var_two = 38
+total   = var_one + var_two
+print(f'The total of variables 1 \nand 2 is {total}')
+
+#computing age using dates of birth
+birth_year = 2020
+current_year = 2025
+age = current_year - birth_year
+print(f'your age is {age}')
+
+#test user-input
+fullname = input('Enter your name:')

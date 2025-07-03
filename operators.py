e29f1bff0a4b10f9791359d43e1493070a8eae1e
xhamdan <magezi.sh@gmail.com>
2025-07-03 13:02:15 +0300

    operators

---
+++ operators.py
@@ -0,0 +1,14 @@
+#program to demonstrate the use of operators.
+#addition
+number_one = 9
+number_two = 2
+mod = number_two % number_one
+print(mod)
+"""sum = number_one + number_two
+sub = number_one - number_two
+product = number_one * number_two
+quotient = number_one / number_two
+
+exponet = number_one ** number_two
+print(f'The Sum is:{sum} \n The Difference is: {sub} \n The Product is: {product} \n The quotient is: {quotient} \nThe Modulus is:{mod} \nThe Power/Exponent is: {exponet}')"""
+

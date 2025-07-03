86ab94c556a064005b5e49b96d539002323db391
xhamdan <magezi.sh@gmail.com>
2025-07-03 12:58:24 +0300

    sum of odd numbers

---
+++ odd_numbers.py
@@ -0,0 +1,8 @@
+#Program to print odd numbers.
+#Remember when you divide an odd number by 2 you get reminder
+odd_number = 0
+while odd_number < 10:
+    if odd_number%2>=1:
+        print(odd_number)
+    odd_number +=1
+print('Terminate')

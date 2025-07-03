d77ae857f43ce6e69d5ec3c5cb40a399f051f9da
xhamdan <magezi.sh@gmail.com>
2025-07-03 11:27:13 +0300

    escape characters

---
+++ escape.py
@@ -0,0 +1,8 @@
+#Introduction to python programming 'hands - on'
+print("\t\t----------------------------------------|")
+print("\t\t|ARIA Student Registration Card\t\t\|")
+print("\t\t|--------------------------------\t|")
+print("\t\t|Student Name:", "\tWalusimbi AbdulHakim\t|\nShahid hamdan")
+print("\t\t|Registration Number:\t",87949887, '\t|')
+print("\t\t|Student Course: \tBIT\t\t|")
+print("\t\t|---------------------------------------|")

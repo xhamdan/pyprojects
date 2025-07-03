8f8f7091236be265e6b735dc21a1a278d7c8057e
xhamdan <magezi.sh@gmail.com>
2025-07-03 11:51:57 +0300

    format strings

---
+++ format.py
@@ -0,0 +1,8 @@
+print(f"Which day did you finsh your assignment:? ", end=' ')
+day = input("")
+
+print(f"which month did you finish your assignment:? ", end=' ')
+month =input()
+
+print(f'She finshed her assignment on {day} in {month}')
+

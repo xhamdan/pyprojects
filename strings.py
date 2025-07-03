8454923bfeed24116924b00d49ddd234196fc854
xhamdan <magezi.sh@gmail.com>
2025-07-03 13:08:30 +0300

    strings

---
+++ strings.py
@@ -0,0 +1,25 @@
+print("Given the numbers 6 and 4")
+#print("The sum of the numbers is:", 6+4)
+#print("The difference of the numbers is:", 6 - 4)
+#print("The product of the numbers is:", 6 * 4)
+#print("The quotient of the numbers:", 6/4)
+#These are so many lines to accomplish a simple task.
+print("Sum:",6+4, "\nDifference:",6-4, "\nProduct:", 6*4)
+
+#Format our string above in better ways using formatted string
+print("Formatted strings Help to join static strings with computed values")
+print(f"Sum: {6+4} \nDifference:{6-4} \nProduct: {6*4} \nBoolean such as 'True/False' Others include:'Set or Dictionary or Sequence'")
+
+#Data-types: Determine what type of data can be stored by a variable.
+print(f"Numerical such as:{4} \nCharacters such as: - or A \nDates: 04/12/2021")
+print(f"")
+
+varp = "hello"
+print(f"Position of e in hello is:{varp.find('e')} \nPosition of l is: {varp.find('l')} \n Hello is {len(varp)} number of words")
+#print(varp.find('e'))
+#print(varp.rfind('l'))
+#print(varp.find('l'))
+
+#https://ariainternationalllc.com/
+#=IF(COUNTIF(C5:H5,TRUE)=5,"Completed","In Progress")
+#=REPT("|",COUNTIF(C4:H4,TRUE)*10)

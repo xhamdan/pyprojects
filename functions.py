e099302446c86a1f80fe910e27fa7b0f71938813
xhamdan <magezi.sh@gmail.com>
2025-07-15 15:44:54 +0300

    functions-rush

---
+++ functions.py
@@ -0,0 +1,5 @@
+# a function related statements that perform a single task. 
+# We declare, define, and call the function to perform the activity
+# how we define a function:: use the keyword 'def function_name('params'):
+# all statements in a function are indented.
+# returns()

f30ee8dcb4cdaa1f31bf7092b98f11f0f46ae67b
xhamdan <magezi.sh@gmail.com>
2025-07-15 18:40:10 +0300

    passing parameters in functions

--- functions.py
+++ functions.py
@@ -3,9 +3,11 @@
 # how we define a function:: use the keyword 'def function_name('params'):
 # all statements in a function are indented.
 # returns()
-def greet():#Declaring a function
-    #Defining a function
-    print("Hello")
-    print("How are you.")
-#calling the function
-greet()
+def greet(name, message):
+    print(f"hello {name}")
+    print(message)
+
+greet('hamudani','How have you been')
+greet('Muhsin','How old are you')
+greet('Yasir','Did you get your own car')
+print('End Program')

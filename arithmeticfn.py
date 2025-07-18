2106205ad4ed0b84c10068b5452daf73fbe4d8a1
xhamdan <magezi.sh@gmail.com>
2025-07-18 15:37:57 +0300

    Creating a sum function to add 2 numbers

---
+++ arithmeticfn.py
@@ -0,0 +1,13 @@
+def sum():
+    number_1 = int(input('Enter first number: '))
+    number_2 = int(input('Enter second number: '))
+    sum = number_1 + number_2
+    print(sum)
+sum()
+
+#alternative way of computing sums
+def sum2(no1,no2): 
+    sum2 = no1+no2
+    print(sum2)
+sum2(10,100)
+sum2(20,15)

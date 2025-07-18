5da630bba23e8554de01058523462160a26eecac
xhamdan <magezi.sh@gmail.com>
2025-07-18 18:36:20 +0300

    arithmet in one place

---
+++ arithmetfunctions.py
@@ -0,0 +1,26 @@
+def sum(number_1, number_2):
+    total = number_1 + number_2
+
+    return(total)
+def subtraction (num_1, num_2):
+    difference = num_1 - num_2 
+
+    return(difference)
+
+def multiplication (num_1, num_2): 
+    product = num_1 * num_2
+
+    return(product)
+
+def division(num_1, numb2):
+    quotent = num_1 /numb2
+
+    return(quotent)
+
+sum_results =sum(10,30)
+subtraction_results=subtraction(14,20)
+quotient_results = division(40,10)
+
+print(sum_results)
+print(subtraction_results)
+print(quotient_results)

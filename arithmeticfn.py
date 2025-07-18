63bd17a52dcc57effa8d1ead5f08435bf611d1c3
xhamdan <magezi.sh@gmail.com>
2025-07-18 16:00:50 +0300

    reuse a function in a program

--- arithmeticfn.py
+++ arithmeticfn.py
@@ -8,6 +8,15 @@ sum()
 #alternative way of computing sums
 def sum2(no1,no2): 
     sum2 = no1+no2
-    print(sum2)
+    
+    return sum2
+
 sum2(10,100)
 sum2(20,15)
+
+#We may need to use the sums above in an activity below to find ----average.
+results = sum2(20,10)
+average = results/2
+
+print(results)
+print(average)

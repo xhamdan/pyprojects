79c6d251c62b08aa4db1b61179815336eb7c80f6
xhamdan <magezi.sh@gmail.com>
2025-07-18 15:38:57 +0300

    adding a disclaimer

--- functionsInput.py
+++ functionsInput.py
@@ -1,8 +1,9 @@
 def greet(name,message):
     print(f"Hello {name}")
-    print(message)
+    print(message, disclaimer)
 
 person = input("Enter Persons Name: ")
 mesaj = input("Enter the message: ")
+disclaimer = input('Provide disclaimer: ')
 
-greet(person,mesaj)
+greet(person,mesaj,disclaimer)

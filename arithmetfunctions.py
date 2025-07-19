118582193f59d2bd4fb0a55cef544ca5dbe2a889
xhamdan <magezi.sh@gmail.com>
2025-07-19 14:33:01 +0300

    Entering values yourself

--- arithmetfunctions.py
+++ arithmetfunctions.py
@@ -1,34 +1,34 @@
 def sum(number_1, number_2):
-    total = number_1 + number_2
+    total = int(number_1) + int(number_2)
 
     return(total)
 def subtraction (num_1, num_2):
-    difference = num_1 - num_2 
+    difference = int(num_1) - int(num_2)
 
     return(difference)
 
 def multiplication (num_1, num_2): 
-    product = num_1 * num_2
+    product = int(num_1) * int(num_2)
 
     return(product)
 
 def division(num_1, numb2):
-    quotent = num_1 /numb2
+    quotent = float(num_1) /float(numb2)
 
     return(quotent)
 
-sum_results =sum(10,30)
-sub_results=subtraction(14,20)
-quo_results = division(40,10)
-
 def average(num1,num2):
     compute_av = sum(num1,num2)
-    average = compute_av/2
+    average = float(compute_av)/2
 
     return average
 
+value_1 = input("First Number: ")
+value_2 = input("Second Number: ")
+
+sum_results =sum(value_1,value_2)
+av_results = average(value_1,value_2)
 
 
 print(f"The Sum of the numbers is {sum_results}")
-print(f" The difference is {sub_results}")
-print(f"The quotient is {quo_results}")
+print(f" The average of the numbers is {av_results}")

6bdc8c115da8bc1cd55affbd9d219fb0ecc2fcca
xhamdan <magezi.sh@gmail.com>
2025-07-21 18:50:47 +0300

    cirumference computed

--- circlefunctions.py
+++ circlefunctions.py
@@ -1,4 +1,4 @@
-#Compute-Area(2TTr2-SA-Circumference2TTr.Diameter(2r)
+#Compute-Area(2TTr2-Circumference2TTr.Diameter(2r)
 
 def compute_diameter(radius):
     diameter = 2 * radius
@@ -10,14 +10,14 @@ def compute_area(radius): #We parse radius as a param bcz its unknown and change
 
     return area 
 
-def compute_surface_area(radius):
-    surface_area = 2 * 3.14 * radius
+def compute_circumference(radius):
+    circumference = 2 * 3.14 * radius
 
-    return surface_area
+    return circumference
 
 
 #compute diameter here while provided the radius
 radius = float(input("Enter value of the Radius: ")) #Same value can be used to find dia,area
 print(f" The diameter is: {compute_diameter(radius)} ")
 print(f" The area of the circle is: {compute_area(radius)} ")
-print(f" The Surface area of the circle is: {compute_surface_area(radius)} ")
+print(f" The Circumference of the circle is: {compute_circumference(radius)} ")
